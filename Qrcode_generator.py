04633fb0328a6c2cdfc38677cc59b3876cb328ef
Mayank <msharma.onetriple9@gmail.com>
2020-03-05 21:56:34 +0530

    Added feature for url

--- Qrcode_generator.py
+++ Qrcode_generator.py
@@ -1,6 +1,15 @@
-#Simple python script that generates qrcode when provided with a url/link
 import qrcode
-url=input("Enter a url/link to generate qrcode: ")
+def convert(url):
+    if url.startswith('http://'):
+        return 'http://' + url[len('http://www.'):]
+    if url.startswith('www.'):
+        return 'https://' + url[len('www.'):]
+    if not url.startswith('https://'):
+        return 'https://www.' + url+".com"
+    return url 
+url=input("Enter a website name/url: ")
+print(convert(url))
 img=qrcode.make(url)
 img.save("Qrcode.png","PNG")  
 print("Qrcode generated")
+
